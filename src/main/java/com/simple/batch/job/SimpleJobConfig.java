62cc7fd617a34ec73a4222fe4f0f0df814aa146e
heyjaja <imjaja09@gmail.com>
2024-02-08 15:01:43 +0900

    BATCH_EXECUTION 실행 데이터 확인

---
+++ src/main/java/com/simple/batch/job/SimpleJobConfig.java
@@ -0,0 +1,54 @@
+package com.simple.batch.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class SimpleJobConfig {
+
+    @Bean
+    public Job simpleJob(JobRepository jobRepository, Step simpleStep1, Step simpleStep2) {
+        return new JobBuilder("simpleJob", jobRepository)
+                .start(simpleStep1)
+                .next(simpleStep2)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+                            @Value("#{jobParameters[requestDate]}") String requestDate) {
+        return new StepBuilder("simpleStep1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>>> This is Step1");
+                    log.info(">>>>>> name = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+                            @Value("#{jobParameters[requestDate]}") String requestDate) {
+        return new StepBuilder("simpleStep2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>>> This is Step2");
+                    log.info(">>>>>> name = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+}
