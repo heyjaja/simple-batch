884025aba8efc57ee033869d07c0db61695e2335
tomato-jaeun <jaja@etomato.com>
2024-02-08 12:17:33 +0900

    Initial commit

---
+++ src/test/java/com/simple/batch/SimpleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
